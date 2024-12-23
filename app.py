af1dc9e050dcaf46e5241ff4d0c473a05567771e
ihebEnetcom <ihebkraini@gmail.com>
2024-12-23 11:27:46 +0100

    Update app.py
    trying  to deploy the app on render service

--- app.py
+++ app.py
@@ -288,6 +288,6 @@ def delete_appointment(id):
 
 
 if __name__ == '__main__':
-    if not os.path.exists(app.config['UPLOAD_FOLDER']):
-        os.makedirs(app.config["UPLOAD_FOLDER"])
-    app.run(debug=True)
+   if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
